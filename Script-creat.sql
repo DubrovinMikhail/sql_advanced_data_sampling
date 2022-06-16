db668d5bd07a7db9fa935297edc17deace13e47f
DubrovinMikhail <mixailgoo@gmail.com>
2022-06-16 22:34:24 +0300

    added creat

--- Script-creat.sql
+++ Script-creat.sql
@@ -30,13 +30,17 @@ create table if not exists track (
 	id serial primary key,
 	name_track varchar(60) not null,
 	albom_id integer references albom(id) not null,
-	duration varchar(60) not null
+	duration integer not null
 );
 
 create table if not exists collection (
 	id serial primary key,
 	name_collection varchar(60) not null,
-	year_of_issue integer,
-	track_id integer references track(id) not null
+	year_of_issue integer
 );
 
+create table if not exists track_collection (
+	track_id integer references track(id),
+	collection_id integer references collection(id),
+	constraint pkart_gen primary key (track_id, collection_id)
+);
